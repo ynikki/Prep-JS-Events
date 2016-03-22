fc0b08ef8e64d590dd98cb1572d47cee1699347c
Victor <viclee02@gmail.com>
2016-03-22 13:31:57 -1000

    add new exercises

---
+++ js/app.js
@@ -0,0 +1,51 @@
+
+
+
+  /*1.  Fill in the correct id to target the paragraph tag*/
+  function fontSize(){
+    document.getElementById(/**/).style.fontSize = "200%";
+  }
+
+ /* 2. change the font color to red*/
+  function colorMeBadd(){
+    
+  }
+
+/*3. fill in the spaces to make this function operate:*/
+  function showAnswer(){
+    document.getElementById(/**/)./**/ = " C (da sea get it?) arrrr!";
+  }
+
+  /*4.  Create a button that will display the message "My name is Lee, Brrruuuce Lee!" in a new paragraph element after the button is clicked.*/
+
+ 
+//Event Listeners
+//Please write your code inside the window.onload function for exerices 5 onward...
+
+window.onload = function (){
+
+document.getElementById(/**/).addEventListener('click', /**/);
+
+function sayQuote(){
+  document.getElementById(/**/).innerHTML = "Our lives are defined by opportunities; even the ones we miss.";
+}
+
+
+//6.  Generate a random quote from clicking on a button
+var quotes = ["It's a funny thing about comin' home. Looks the same, smells the same, feels the same. You'll realize what's changed is you.", "Momma? Momma? Some days, I feel different than the day before.", "Some people, were born to sit by a river. Some get struck by lightning. Some have an ear for music. Some are artists. Some swim. Some know buttons. Some know Shakespeare. Some are mothers. And some people, dance."];
+
+document.getElementById(/**/).addEventListener(/**/, sayQuotes);
+                                                   
+function sayQuotes(){
+  var getQuotes = quotes[Math.floor(Math.random()*quotes.length)];
+  document.getElementById(/**/).innerHTML = /**/;
+}
+
+/*7. Create your own event listener*/
+
+
+};
+
+
+
+
